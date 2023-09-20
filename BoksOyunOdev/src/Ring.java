337a18ea5eb1460fe025b218fd919310be52c182
batuhanlog <mrkayahan66@gmail.com>
2023-09-21 02:29:30 +0300

    Boks Oyunu Prog

---
+++ BoksOyunOdev/src/Ring.java
@@ -0,0 +1,52 @@
+public class Ring {
+    Fighter f1;
+    Fighter f2;
+    int minWeight;
+    int maxWeight;
+
+    public Ring(Fighter f1, Fighter f2, int minWeight, int maxWeight) {
+        this.f1 = f1;
+        this.f2 = f2;
+        this.minWeight = minWeight;
+        this.maxWeight = maxWeight;
+
+        // İlk kimin dövüşe başlayacağını %50 olasılıkla belirle
+        if (Math.random() < 0.5) {
+            Fighter temp = this.f1;
+            this.f1 = this.f2;
+            this.f2 = temp;
+        }
+    }
+
+    public void run() {
+        if (checkWeight()) {
+            while (f1.health > 0 && f2.health > 0) {
+                System.out.println("======== YENİ ROUND ===========");
+                f2.health = f1.hit(f2);
+                if (isWin()) {
+                    break;
+                }
+                f1.health = f2.hit(f1);
+                if (isWin()) {
+                    break;
+                }
+                printScore();
+            }
+        } else {
+            System.out.println("Sporcuların ağırlıkları uyuşmuyor.");
+        }
+    }
+
+    public boolean checkWeight() {
+        return (f1.weight >= minWeight && f1.weight <= maxWeight) && (f2.weight >= minWeight && f2.weight <= maxWeight);
+    }
+
+    // Diğer Ring sınıfı metotları burada
+
+    public static void main(String[] args) {
+        Fighter marc = new Fighter("Marc", 15, 100, 90, 0);
+        Fighter alex = new Fighter("Alex", 10, 95, 100, 0);
+        Ring r = new Ring(marc, alex, 90, 100);
+        r.run();
+    }
+}
