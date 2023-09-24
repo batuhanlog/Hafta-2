ef00a1e8258314563b1c0a5327454b755a93b918
batuhanlog <mrkayahan66@gmail.com>
2023-09-24 23:10:54 +0300

    cift sayı

--- CiftSayilar/src/Main.java
+++ CiftSayilar/src/Main.java
@@ -5,18 +5,18 @@ public class Main {
     public static void main(String[] args) {
         int[] sayiDizisi = {2, 3, 4, 4, 6, 8, 8, 10, 10, 12, 12};
 
-        // Çift sayıları ve tekrarlarını saymak için bir Map kullanalım
+
         Map<Integer, Integer> tekrarlar = new HashMap<>();
 
         for (int sayi : sayiDizisi) {
-            // Eğer sayı çiftse
+
             if (sayi % 2 == 0) {
-                // Map'te bu sayının tekrarını arttıralım
+
                 tekrarlar.put(sayi, tekrarlar.getOrDefault(sayi, 0) + 1);
             }
         }
 
-        // Sonuçları ekrana yazdır
+
         System.out.println("Çift Sayılar ve Tekrar Sayıları:");
         for (Map.Entry<Integer, Integer> entry : tekrarlar.entrySet()) {
             System.out.println(entry.getKey() + " tekrar ediyor: " + entry.getValue() + " kez");
